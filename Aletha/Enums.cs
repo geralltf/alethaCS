62c91e7051c4c72fbaa3b956bc9e2f222248a3c8
Gerallt Franke <geralltf@gmail.com>
2016-08-14 19:42:43 +1000

    Implemented NavigationType

---
+++ Aletha/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aletha
+{
+    public enum NavigationType
+    {
+        Walk = 0,
+        Fly = 1,
+        _LastType = 2
+    }
+}
